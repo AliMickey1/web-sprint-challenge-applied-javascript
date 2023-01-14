15bd55faa127d946a48696ab168ff071685f2a7d
Aliceson Phillips <alicesonphillips@gmail.com>
2023-01-14 10:52:55 -0500

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import { container } from "webpack"
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +19,32 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const card = document.createElement('div')
+  const headline = document.createElement('div')
+  const author = document.createElement('div')
+  const imgCont = document.createElement('div')
+  const authorImage = document.createElement('img')
+  const nameAuthor = document.createElement('span')
+
+  card.classList.add('card')
+  headline.classList.add('headline')
+  author.classList.add('author')
+  imgCont.classList.add('img-container')
+  authorImage.src = article.authorPhoto
+  
+  headline.textContent = article.headline
+  nameAuthor.textContent = article.authorName
+
+  card.appendChild(headline)
+  card.appendChild(author)
+  author.appendChild(imgCont)
+  imgCont.appendChild(authorImage)
+  author.appendChild(nameAuthor)
+
+  card.addEventListener('click', () => {
+    
+  })
+
 }
 
 const cardAppender = (selector) => {
